7c3d5da4de1013b6444dd9e60a28bba6830b9b7b
dingpanpan <dingpanpan@yuewen.com>
2018-05-23 12:59:17 +0800

    更改目录

---
+++ tars/tars.proto.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'appName' => 'PHPTest',
+    'serverName' => 'swoft',
+    'objName' => 'obj',
+    'withServant' => true,//决定是服务端,还是客户端的自动生成
+    'jceFiles' => array(
+        'jce/example.jce'
+    ),
+    'srcPath' => '',//这里用来autoload
+    'namespace' => '',
+);
